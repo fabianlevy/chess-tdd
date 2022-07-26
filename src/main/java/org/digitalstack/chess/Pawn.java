a30720a088808c7b3d4a846a63a8226e99055a0c
LarisaBardita <48147940+LarisaBardita@users.noreply.github.com>
2022-07-26 23:12:33 +0300

    Update Pawn.java

--- src/main/java/org/digitalstack/chess/Pawn.java
+++ src/main/java/org/digitalstack/chess/Pawn.java
@@ -44,7 +44,12 @@ public class Pawn {
     }
 
     public void move(MovementType movementType, int newX, int newY) {
-        throw new UnsupportedOperationException("Need to implement Pawn.move()") ;
+         if(movementType == MovementType.MOVE) {
+            if(newX == this.getXCoordinate() && newY == this.getYCoordinate()-1) {
+                this.setXCoordinate(newX);
+                this.setYCoordinate(newY);
+            }
+        }
     }
 
     @Override
