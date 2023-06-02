32ddaca8b39105cfc33e75830df6bfb1ca1e04df
Alexia Iliescu <94546611+iliescualexia@users.noreply.github.com>
2023-06-02 14:02:27 +0300

    first commit

--- src/main/java/org/digitalstack/chess/Pawn.java
+++ src/main/java/org/digitalstack/chess/Pawn.java
@@ -39,12 +39,53 @@ public class Pawn {
         return this.pieceColor;
     }
 
-    private void setPieceColor(PieceColor value) {
+    public void setPieceColor(PieceColor value) {
         pieceColor = value;
     }
 
     public void move(MovementType movementType, int newX, int newY) {
-        throw new UnsupportedOperationException("Need to implement Pawn.move()") ;
+        if(chessBoard.isLegalBoardPosition(newX,newY)){
+            if(pieceColor==PieceColor.BLACK) /// starts at row 0
+            {
+                if (movementType.equals(MovementType.MOVE) && chessBoard.getPieces()[newX][newY] == null) {
+                    if (newY == yCoordinate - 1 && newX== xCoordinate) {
+                        xCoordinate = newX;
+                        yCoordinate = newY;
+                        System.out.println("Moved black pawn to (" + newX + ", " + newY + ")");
+                    } else {
+                        System.out.println("Invalid move");
+                    }
+                } else if (movementType.equals(MovementType.CAPTURE) && chessBoard.getPieces()[newX][newY].pieceColor == PieceColor.WHITE) {
+                    if (newY == yCoordinate - 1 && Math.abs(newX - xCoordinate) == 1) {
+                        xCoordinate = newX;
+                        yCoordinate = newY;
+                        System.out.println("Black piece captured a white one at position ");
+                    }else{
+                        System.out.println("Invalid capture");
+                    }
+                }
+            } else if (pieceColor==PieceColor.WHITE) { ///start ar row 6
+                if (movementType.equals(MovementType.MOVE) && chessBoard.getPieces()[newX][newY] == null) {
+                    if (newY == yCoordinate + 1 && newX == xCoordinate) {
+                        xCoordinate = newX;
+                        yCoordinate = newY;
+                        System.out.println("Moved white pawn to (" + newX + ", " + newY + ")");
+                    } else {
+                        System.out.println("Invalid move");
+                    }
+                } else if (movementType.equals(MovementType.CAPTURE) && chessBoard.getPieces()[newX][newY].pieceColor == PieceColor.BLACK) {
+                    if (newY == yCoordinate + 1 && Math.abs(newX - xCoordinate) == 1) {
+                        xCoordinate = newX;
+                        yCoordinate = newY;
+                        System.out.println("White piece captured a black one at position ");
+                    }else{
+                        System.out.println("Invalid capture");
+                    }
+                }
+            }
+
+        }
+       // throw new UnsupportedOperationException("Need to implement Pawn.move()") ;
     }
 
     @Override
