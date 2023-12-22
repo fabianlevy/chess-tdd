753ffbc859b5ebfeeb4626647f72631c363e0331
AndradaHr <hrubanandrada@gmail.com>
2023-12-22 16:20:24 +0200

    add,isLegalBoardPosition and move methods completed

--- src/main/java/org/digitalstack/chess/ChessBoard.java
+++ src/main/java/org/digitalstack/chess/ChessBoard.java
@@ -12,10 +12,32 @@ public class ChessBoard {
     }
 
     public void add(Pawn pawn, int xCoordinate, int yCoordinate, PieceColor pieceColor) {
-        throw new UnsupportedOperationException("Need to implement ChessBoard.add()");
+        // check if we are inside the chessboard boundaries and not already occupied
+        // and then place the pawn and set the coordinates and color for it
+        if (isLegalBoardPosition(xCoordinate, yCoordinate) && pieces[xCoordinate][yCoordinate] == null ) {
+            pieces[xCoordinate][yCoordinate] = pawn;
+            pawn.setPieceColor(pieceColor);
+            pawn.setChessBoard(this);
+            pawn.setXCoordinate(xCoordinate);
+            pawn.setYCoordinate(yCoordinate);
+        } else { //  invalidate the pawn's coordinates
+            pawn.setXCoordinate(-1);
+            pawn.setYCoordinate(-1);
+        }
     }
 
+
     public boolean isLegalBoardPosition(int xCoordinate, int yCoordinate) {
-        throw new UnsupportedOperationException("Need to implement ChessBoard.IsLegalBoardPosition()");
+        // check if we are inside the chessboard boundaries
+        return (xCoordinate >= 0 && xCoordinate < BOARD_WIDTH) && (yCoordinate >= 0 && yCoordinate < BOARD_HEIGHT);
     }
+
+    // checking the state of a specific position on the chessboard
+    public Pawn getPieceAt(int x, int y) {
+        if (isLegalBoardPosition(x, y)) {
+            return pieces[x][y];
+        }
+        return null;
+    }
+
 }
--- src/main/java/org/digitalstack/chess/Pawn.java
+++ src/main/java/org/digitalstack/chess/Pawn.java
@@ -39,13 +39,35 @@ public class Pawn {
         return this.pieceColor;
     }
 
-    private void setPieceColor(PieceColor value) {
+    void setPieceColor(PieceColor value) {
         pieceColor = value;
     }
 
-    public void move(MovementType movementType, int newX, int newY) {
-        throw new UnsupportedOperationException("Need to implement Pawn.move()") ;
+    private boolean isOpponentPieceAt(int x, int y) {
+        //check if a specific square on the chessboard is occupied by an opponent's piece
+        Pawn piece = chessBoard.getPieceAt(x, y);
+        return piece != null && piece.getPieceColor() != this.pieceColor;
     }
+
+    public void move(MovementType movementType, int newX, int newY) {
+
+        if (!chessBoard.isLegalBoardPosition(newX, newY)) {return;}
+        int yDirection = (pieceColor == PieceColor.WHITE) ? 1 : -1; //set the direction in which the pawn should move
+        //handle standard pawn movement
+        if (movementType == MovementType.MOVE) {
+            boolean isOneStepMove = (newY - yCoordinate == yDirection) && newX == xCoordinate;
+            if (isOneStepMove) {
+                setYCoordinate(newY);
+                setXCoordinate(newX);}
+        }//then, handle pawns capture
+        else if (movementType == MovementType.CAPTURE) {
+            boolean isDiagonalCapture = Math.abs(newX - xCoordinate) == 1 && newY - yCoordinate == yDirection;
+            if (isDiagonalCapture && isOpponentPieceAt(newX, newY)) {
+                setXCoordinate(newX);
+                setYCoordinate(newY);
+        }
+    }
+}
 
     @Override
     public String toString() {
