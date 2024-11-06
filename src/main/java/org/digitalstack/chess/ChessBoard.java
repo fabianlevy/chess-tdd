8475f77462631c49b25252a019a072a4f32ebfd1
Madalina Avram <madalina.avram@fanduel.com>
2024-11-06 23:08:59 +0200

    homework

--- src/main/java/org/digitalstack/chess/ChessBoard.java
+++ src/main/java/org/digitalstack/chess/ChessBoard.java
@@ -1,5 +1,7 @@
 package org.digitalstack.chess;
 
+import java.util.Arrays;
+
 public class ChessBoard {
 
     public static int BOARD_WIDTH = 7;
@@ -11,11 +13,31 @@ public class ChessBoard {
         pieces = new Pawn[BOARD_WIDTH][BOARD_HEIGHT];
     }
 
-    public void add(Pawn pawn, int xCoordinate, int yCoordinate, PieceColor pieceColor) {
-        throw new UnsupportedOperationException("Need to implement ChessBoard.add()");
+    public void add( Pawn pawn, int xCoordinate, int yCoordinate, PieceColor pieceColor ) {
+        // Verificam daca pozitia pionului e valida
+        if ( isLegalBoardPosition( xCoordinate, yCoordinate ) ) {
+            // Verificam daca pozitia e ocupata
+            if ( pieces[xCoordinate][yCoordinate] != null ) {
+
+                pawn.setXCoordinate( -1 );
+                pawn.setYCoordinate( -1 );
+                return;
+            }
+
+            // Daca pozitia e libera punem pionul la coordonatele x,y
+            pieces[xCoordinate][yCoordinate] = pawn;
+            pawn.setXCoordinate( xCoordinate );
+            pawn.setYCoordinate( yCoordinate );
+        } else {
+            pawn.setXCoordinate( -1 );
+            pawn.setYCoordinate( -1 );
+        }
     }
 
-    public boolean isLegalBoardPosition(int xCoordinate, int yCoordinate) {
-        throw new UnsupportedOperationException("Need to implement ChessBoard.IsLegalBoardPosition()");
+    public boolean isLegalBoardPosition( int xCoordinate, int yCoordinate ) {
+        if ( xCoordinate < 0 || yCoordinate < 0 || xCoordinate > 6 || yCoordinate > 6 ) {
+            return false;
+        }
+        return true;
     }
 }
