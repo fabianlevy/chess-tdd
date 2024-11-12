3060ac836cd88b84bb83491beb59c9d6101dc04e
ElenaRodicaDoran <rodica-elena.doran@ppb.com>
2024-11-12 18:46:41 +0200

    new

--- src/main/java/org/digitalstack/chess/Pawn.java
+++ src/main/java/org/digitalstack/chess/Pawn.java
@@ -44,7 +44,7 @@ public class Pawn {
     }
 
     public void move(MovementType movementType, int newX, int newY) {
-        throw new UnsupportedOperationException("Need to implement Pawn.move()") ;
+        //throw new UnsupportedOperationException("Need to implement Pawn.move()") ;
     }
 
     @Override
